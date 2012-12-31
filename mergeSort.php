25bcd3fddc0f1a5d66ce9aa455f54dde07ec643c
catmanol <catmanol@yahoo.com>
2012-12-31 14:59:04 +0200

    Added comment to merge sort

--- mergeSort.php
+++ mergeSort.php
@@ -1,21 +1,23 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of mergeSort
  *
  * @author cata
  */
 class mergeSort {
-    public $array=array();
+    /**
+     * splitting the array recursively until has max two elements
+     * merging and sorting recursively until sorted array is built
+     * 
+     * @param type $array
+     * @return array Sorted input array
+     */
     public function sort($array) {
+        //if array has 1 element return that element
         $count=count($array);
         if ($count<=1)
             return $array;
+        //split array and build left and right arrays
         $mid=(int)$count/2;
         $left=array();
         $right=array();
@@ -25,10 +27,21 @@ class mergeSort {
         for ($i=$mid;$i<$count;$i++) {
             $right[]=$array[$i];
         }
+        //recursively split the left and right arrays
+        //sort and merge recursively left and right arrays
         return $this->merge($this->sort($left),$this->sort($right));
     }
+    /**
+     * merge left and right arrays sorting elements
+     * 
+     * @param array $left
+     * @param array $right
+     * @return array
+     */
     private function merge($left,$right) {
         $sorted=array();
+        //compare first element from left with first element from right
+        //add the lowest to $sorted and erase it from parent array
         while (count($left)>0 && count($right)>0) 
             if ($left[0]<$right[0]) {
                 $sorted[]=$left[0];
@@ -37,37 +50,12 @@ class mergeSort {
                 $sorted[]=$right[0];
                 array_shift($right);
             }
-            print_r($sorted);
-            echo "<br/>";
+        //after one of right and left arrays is empty is added elements from
+        //remaining to $sorted
         for ($i=0;$i<count($left);$i++) 
             $sorted[]=$left[$i];
         for ($i=0;$i<count($right);$i++) 
             $sorted[]=$right[$i];
         return $sorted;
-    }
-    public function fun($array) {
-        $length=count($array);
-        $count=1;
-        while ($length!=$count) {
-            $max=isset($max)?$max:$length;
-            for ($i=0;$i<$max;$i=$i+2) {
-                $array[$i]=is_array($array[$i])?$array[$i]:array($array[$i]);
-                if (isset($array[$i+1])) {
-                    $array[$i+1]=is_array($array[$i+1])?$array[$i+1]:array($array[$i+1]);
-                    $array[(int)$i/2]=$this->merge($array[$i],$array[$i+1]);
-                } else {
-                    $array[(int)$i/2]=$array[$i];
-                }
-                unset($array[$i+1]);
-                if ($i!=0) { unset($array[$i]); }
-            }
-            print_r($array[0]);
-            $max=ceil($max/2);
-            echo $max."<br/>";
-            //$array=array_splice($array,$max-1);
-            print_r($array);
-            $count=count($array[0]);
-        }
-        return $array[0];
     }
 }
